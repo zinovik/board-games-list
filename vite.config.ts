4bcdaf15d52dfecce91f8b49d04759c51bff25a3
Max Zinovik <zinovik@gmail.com>
2026-04-13 19:59:30 +0200

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/board-games-list/',
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
 })
